401bc4364dcdcdc73a29776ee4c161d882956cef
Sakibur Rahman <122334294+Sakib70708@users.noreply.github.com>
2023-01-23 17:52:05 +0600

    Add files via upload

---
+++ Lab-02/1.429_49-07.cpp
@@ -0,0 +1,22 @@
+//codeforce problem No:231A
+//Problem Name:Team
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,a,b,c,count=0;
+    cin >> x;
+    for(int i=0;i<x;i++)
+    {
+        cin>>a>>b>>c;
+        if(a+b+c>=2)
+        {
+            count++;
+        }
+
+    }
+    cout << count<< endl;
+    return 0;
+}
