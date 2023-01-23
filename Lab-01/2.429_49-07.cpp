310fb13616d97bb8ad53725938050920a217ddfe
Sakibur Rahman <122334294+Sakib70708@users.noreply.github.com>
2023-01-23 17:39:36 +0600

    Add files via upload

---
+++ Lab-01/2.429_49-07.cpp
@@ -0,0 +1,24 @@
+//codeforce problem No:71A
+//Problem Name:Way Too Long Words
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    string s;
+    cin >> n;
+    while (n--)
+    {
+        cin >> s;
+        if (s.length() > 10)
+        {
+            cout << s[0] << s.length() - 2 << s[s.length() - 1] << endl;
+        }
+        else
+        {
+            cout << s << endl;
+        }
+    }
+    return 0;
+}
