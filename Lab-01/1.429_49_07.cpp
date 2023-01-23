572185184ec77079422ac35658c68b22767e9166
Sakibur Rahman <122334294+Sakib70708@users.noreply.github.com>
2023-01-23 17:14:15 +0600

    Add files via upload

---
+++ Lab-01/1.429_49_07.cpp
@@ -0,0 +1,21 @@
+//codeforce problem No:4A
+//Problem Name:Watermelon
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int w;
+    cin >> w;
+    if (w % 2 == 0 && w > 2)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
