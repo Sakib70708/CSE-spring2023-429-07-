798b0e06e228924145f5aeb4d4101da371f94858
Sakibur Rahman <122334294+Sakib70708@users.noreply.github.com>
2023-02-13 14:36:25 +0600

    Add files via upload

---
+++ Lab-5/10.429_49_07.cpp
@@ -0,0 +1,15 @@
+//codeforce problem No:339A
+//Problem Name:Helpful Maths
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    s[0] = towupper(s[0]);
+    cout << s << endl;
+    return 0;
+}
